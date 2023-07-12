4b58246cf176e574116576eb4a650a34b9427b87
Erik Mendes <erik.mendes19@gmail.com>
2023-07-12 12:59:16 -0300

    Instalão do Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:pro_angler/Providers/user_provider.dart';
 import 'package:pro_angler/Views/home_page.dart';
 import 'package:pro_angler/Views/login_page.dart';
 import 'package:pro_angler/Views/my_team_page.dart';
@@ -7,35 +7,17 @@ import 'package:pro_angler/Views/my_tournaments_page.dart';
 import 'package:pro_angler/Views/profile_page.dart';
 import 'package:pro_angler/Views/signup_page.dart';
 import 'package:pro_angler/Views/tournaments_page.dart';
-import 'package:provider/provider.dart';
 
-import 'Providers/team_provider.dart';
-import 'Providers/tournament_provider.dart';
 import 'Views/new_tournament_page.dart';
 
-void main() {
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider<TournamentProvider>(
-          create: (_) => TournamentProvider(),
-        ),
-        ChangeNotifierProvider<TeamProvider>(
-          create: (_) => TeamProvider(),
-        ),
-        ChangeNotifierProvider<UserProvider>(
-          create: (_) => UserProvider(),
-        ),
-        
-      ],
-      child: const MyApp(),
-    ),
-  );
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
